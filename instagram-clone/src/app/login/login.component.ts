2910d3031fbd601121bef0ffed3936b93004000c
unknown <thruptikadenga@gmail.com>
2023-03-23 15:34:02 +0530

    instagram login page clone

---
+++ instagram-clone/src/app/login/login.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent implements OnInit {
+  loginForm: FormGroup | any;
+  loading = false;
+  submitted = false;
+  
+  constructor(
+    private formBuilder: FormBuilder,
+
+  ) { }
+
+  ngOnInit(): void {
+    this.loginForm = this.formBuilder.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required]
+  });
+  }
+  get f() { return this.loginForm.controls; }
+  onSubmit() {
+    this.submitted = true;
+    if (this.loginForm.invalid) {
+        return;
+    }
+
+    this.loading = true;
+}
+}
